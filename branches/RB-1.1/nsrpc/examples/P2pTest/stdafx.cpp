5b0857fd0fad2c4528dc13c0fc1b486cfd6ae0e8
kcando <kcando@6d7ccee0-1a3b-0410-bfa1-83648d9ec9a4>
2007-09-28 02:58:40 +0000

    Removed file/folder

--- branches/RB-1.1/nsrpc/examples/P2pTest/stdafx.cpp
+++
@@ -1,5 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// P2pTest.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
