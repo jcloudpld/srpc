0c8b0b2783b100c91c186f7b5713afebe405eb95
kcando <kcando@6d7ccee0-1a3b-0410-bfa1-83648d9ec9a4>
2007-09-28 01:28:45 +0000

    Release 1.1.1

---
+++ tags/REL-1.1.1/nsrpc/test/nsrpcTest/TestClientSession.h
@@ -0,0 +1,44 @@
+#ifndef NSRPC_TESTCLIENTSESSION_H
+#define NSRPC_TESTCLIENTSESSION_H
+
+#include <nsrpc/detail/ClientSession.h>
+
+/**
+ * @class TestClientSession
+ * 테스트용 ClientSession
+ */
+class TestClientSession : public nsrpc::ClientSession
+{
+public:
+    TestClientSession(ACE_Reactor* reactor) :
+        nsrpc::ClientSession(reactor),
+        disconnected_(false),
+        recvSize_(0) {}
+
+    bool isDisconnected() const {
+        return disconnected_;
+    }
+
+    size_t getRecvSize() const {
+        return recvSize_;
+    }
+private:
+    virtual void onConnected() {}
+    virtual void onDisconnected() {
+        disconnected_ = true;
+    }
+    virtual void onMessageArrived(nsrpc::CsMessageType /*messageType*/) {
+        recvSize_ += acquireRecvBlock().length();
+    }
+public: // for Test
+    virtual ACE_Message_Block& acquireSendBlock() {
+        return nsrpc::ClientSession::acquireSendBlock();
+    }
+    //virtual ACE_Message_Block& acquireRecvBlock();
+    //virtual void release(ACE_Message_Block& mblock);
+private:
+    bool disconnected_;
+    size_t recvSize_;
+};
+
+#endif // !defined(NSRPC_TESTCLIENTSESSION_H)
