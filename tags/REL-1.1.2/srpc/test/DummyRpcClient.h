b704719ae00463aafba64adb5214f843e3c6fe80
kcando <kcando@6d7ccee0-1a3b-0410-bfa1-83648d9ec9a4>
2007-10-04 01:37:47 +0000

    Tag REL-1.1.2

---
+++ tags/REL-1.1.2/srpc/test/DummyRpcClient.h
@@ -0,0 +1,35 @@
+#ifndef SRPC_DUMMYRPCCLIENT_H
+#define SRPC_DUMMYRPCCLIENT_H
+
+#include "DummyRpcInterface.h"
+#include <srpc/RpcForwarder.h>
+#include <srpc/RpcReceiver.h>
+
+/**
+ * @class DummyRpcClient
+ */
+class DummyRpcClient : public DummyRpc,
+    private srpc::RpcReceiver, private srpc::RpcForwarder
+{
+    DECLARE_SRPC_EVENT_DISPATCHER(DummyRpcClient);
+public:
+    DummyRpcClient(srpc::RpcNetwork* rpcNetwork = 0);
+
+private: // encapsulation
+    DECLARE_SRPC_METHOD_0(rpc0);
+    DECLARE_SRPC_METHOD_1(rpc1, RInt32, p1);
+    DECLARE_SRPC_METHOD_2(rpc2, RInt32, p1, RInt32, p2);
+    DECLARE_SRPC_METHOD_3(rpc3, RInt32, p1, RInt32, p2, RInt32, p3);
+    DECLARE_SRPC_METHOD_4(rpc4, RInt32, p1, RInt32, p2, RInt32, p3,
+        RInt32, p4);
+    DECLARE_SRPC_METHOD_5(rpc5, RInt32, p1, RInt32, p2, RInt32, p3,
+        RInt32, p4, RInt32, p5);
+    DECLARE_SRPC_METHOD_6(rpc6, RInt32, p1, RInt32, p2, RInt32, p3,
+        RInt32, p4, RInt32, p5, RInt32, p6);
+    DECLARE_SRPC_METHOD_7(rpc7, RInt32, p1, RInt32, p2, RInt32, p3,
+        RInt32, p4, RInt32, p5, RInt32, p6, RInt32, p7);
+    DECLARE_SRPC_METHOD_2(rpcBits, RInt15, p1, RInt31, p2);
+    DECLARE_SRPC_METHOD_0(rpcFailed);
+};
+
+#endif // SRPC_DUMMYRPCCLIENT_H
