9fb976aeade3b2228755eb403b6f27f327b7e9ae
kcando <kcando@6d7ccee0-1a3b-0410-bfa1-83648d9ec9a4>
2008-02-05 02:01:18 +0000

    Fix header file inclusion

--- nsrpc/include/nsrpc/Session.h
+++ nsrpc/include/nsrpc/Session.h
@@ -6,6 +6,18 @@
 #include "detail/MessageBlockProvider.h"
 #include "detail/CsProtocol.h"
 #include <srpc/StringTypes.h>
+#ifdef _MSC_VER
+#  pragma warning (push)
+#  pragma warning (disable: 4127 4244 4251 4267 4312 4996)
+#endif
+#ifdef USE_TPROACTOR
+#  include <TProactor/Asynch_IO.h>
+#else
+#  include <ace/Asynch_IO.h>
+#endif // USE_TPROACTOR
+#ifdef _MSC_VER
+#  pragma warning (pop)
+#endif
 #include <ace/INET_Addr.h>
 #include <ace/Atomic_Op.h>
 #include <ace/Thread_Mutex.h>
