2b703259f168aa034c260662ee30122f0ea4fd81
kcando <kcando@6d7ccee0-1a3b-0410-bfa1-83648d9ec9a4>
2008-02-23 05:43:46 +0000

    P2P 최대 타임아웃 시간 변경 (15->10)

--- trunk/nsrpc/include/nsrpc/p2p/P2pConfig.h
+++ trunk/nsrpc/include/nsrpc/p2p/P2pConfig.h
@@ -21,7 +21,7 @@ struct P2pConfig
         peerPingInterval = 500,
         peerRoundTripTimeoutFactor = 2,
         peerRoundTripTimeoutLimitFactor = 10,
-        peerMaxDisconnectTimeout = 15000,
+        peerMaxDisconnectTimeout = 10000,
         peerMinDisconnectTimeout = 5000,
 
         /// default Maximum Transmission Unit 
