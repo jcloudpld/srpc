693bcf221273d57f8ec7c365d7d6833592934c98
kcando <kcando@6d7ccee0-1a3b-0410-bfa1-83648d9ec9a4>
2008-12-09 04:12:03 +0000

    release REL-1.6.20

---
+++ tags/REL-1.6.20/srpc/include/srpc/detail/Allocator.h
@@ -0,0 +1,40 @@
+#ifndef SRPC_ALLOCATOR_H
+#define SRPC_ALLOCATOR_H
+
+#include "../config/config.h"
+#include <memory>
+
+#if defined(USE_BOOST_POOL_ALLOCATOR_FOR_SRPC) || \
+    defined(USE_BOOST_FAST_POOL_ALLOCATOR_FOR_SRPC) || \
+    defined(USE_BOOST_POOL_ALLOCATOR_FOR_SRPC_STRING)
+
+#ifdef _MSC_VER
+#  pragma warning (push)
+#  pragma warning (disable: 4127)
+#endif
+#include <boost/pool/pool_alloc.hpp>
+#ifdef _MSC_VER
+#  pragma warning (pop)
+#endif
+
+#endif
+
+#ifdef USE_BOOST_POOL_ALLOCATOR_FOR_SRPC
+#  define SrpcNormalAllocator boost::pool_allocator
+#else
+#  define SrpcNormalAllocator std::allocator
+#endif
+
+#ifdef USE_BOOST_FAST_POOL_ALLOCATOR_FOR_SRPC
+#  define SrpcNodeAllocator boost::fast_pool_allocator
+#else
+#  define SrpcNodeAllocator std::allocator
+#endif
+
+#ifdef USE_BOOST_POOL_ALLOCATOR_FOR_SRPC_STRING
+#  define SrpcStringAllocator boost::pool_allocator
+#else
+#  define SrpcStringAllocator std::allocator
+#endif
+
+#endif // SRPC_ALLOCATOR_H
