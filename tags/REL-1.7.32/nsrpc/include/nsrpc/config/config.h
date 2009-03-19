f10dafd3069b1a889757e0108938ce54aa4ca6a8
kcando <kcando@6d7ccee0-1a3b-0410-bfa1-83648d9ec9a4>
2009-03-19 11:44:38 +0000

    release 1.17.32

---
+++ tags/REL-1.7.32/nsrpc/include/nsrpc/config/config.h
@@ -0,0 +1,16 @@
+#ifndef NSRPC_CONFIG_H
+#define NSRPC_CONFIG_H
+
+#include <srpc/config/config.h>
+
+#if defined (_MSC_VER)
+#   include "config_msvc.h"
+#elif defined(__GNUC__)
+#   include "config_gcc.h"
+#else
+#   error 현재는 MSVC와 GCC만 지원합니다
+#endif
+
+#define USE_VARIOUS_MEMORY_ALLOCATOR_IN_MESSAGE_BLOCK_MANAGER
+
+#endif // NSRPC_CONFIG_H
