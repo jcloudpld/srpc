371e9648543a9ddfe72e7b31403f96ceb1b4aad1
kcando <kcando@6d7ccee0-1a3b-0410-bfa1-83648d9ec9a4>
2009-03-24 14:16:31 +0000

    CHG: reverted

--- nsrpc/src/SmartPtr.cpp
+++ nsrpc/src/SmartPtr.cpp
@@ -4,6 +4,32 @@
 
 namespace nsrpc {
 
+namespace {
+
+template <typename T>
+void AtomicIncrement(T* value)
+{
+#if defined( WIN32 ) || defined( _WIN32 ) || defined( __WIN32__ )
+    (void)BOOST_INTERLOCKED_INCREMENT(reinterpret_cast<long*>(value));
+#elif defined( __GNUC__ ) && ( defined( __i386__ ) || defined( __x86_64__ ) )
+    (void)boost::detail::atomic_conditional_increment(value);
+#endif
+}
+
+
+/// @return 연산 후 (value == 0)이면 true
+template <typename T>
+bool AtomicDecrement(T* value)
+{
+#if defined( WIN32 ) || defined( _WIN32 ) || defined( __WIN32__ )
+    return BOOST_INTERLOCKED_DECREMENT(reinterpret_cast<long*>(value)) == 0;
+#elif defined( __GNUC__ ) && ( defined( __i386__ ) || defined( __x86_64__ ) )
+    return boost::detail::atomic_exchange_and_add(value) == 1;
+#endif
+}
+
+} // namespace
+
 void MTSmartPtrTraits::addReference(const SharedObject* rawPtr)
 {
     if (rawPtr != 0) {
