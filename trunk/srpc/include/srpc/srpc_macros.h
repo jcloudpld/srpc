0bf26e2c32c78b5d67b831f2efc84f595ce14e8b
kcando <kcando@6d7ccee0-1a3b-0410-bfa1-83648d9ec9a4>
2009-03-31 05:46:10 +0000

    CHG: replace SRPC_UNUSED_ARG

--- trunk/srpc/include/srpc/srpc_macros.h
+++ trunk/srpc/include/srpc/srpc_macros.h
@@ -5,28 +5,10 @@
 #pragma once
 #endif
 
-// stolen from ACE
-#if defined (ghs) || defined (__GNUC__) || defined (__hpux) || defined (__sgi) || defined (__DECCXX) || defined (__rational__) || defined (__USLC__) || defined (SRPC_RM544) || defined (__DCC__) || defined (__PGI) || defined (__TANDEM)
-// Some compilers complain about "statement with no effect" with (a).
-// This eliminates the warnings, and no code is generated for the null
-// conditional statement.  @note that may only be true if -O is enabled,
-// such as with GreenHills (ghs) 1.8.8.
-#  define SRPC_UNUSED_ARG(a) do {/* null */} while (&a == 0)
+// stolen from boost/statechart/detail/avoid_unused_warning.hpp
+template< typename T >
+inline void avoid_unused_warning(const T &) {}
 
-#elif defined (__DMC__)
-
-#  define SRPC_UNUSED_ID(identifier)
-template <class T>
-inline void SRPC_UNUSED_ARG(const T& SRPC_UNUSED_ID(t)) { }
-
-#else /* ghs || __GNUC__ || ..... */
-#  define SRPC_UNUSED_ARG(a) (a)
-#endif /* ghs || __GNUC__ || ..... */
-
-#if defined (_MSC_VER) || defined(__sgi) || defined (ghs) || defined (__DECCXX) || defined(__BORLANDC__) || defined (SRPC_RM544) || defined (__USLC__) || defined (__DCC__) || defined (__PGI) || defined (__TANDEM) || (defined (__HP_aCC) && (__HP_aCC >= 60500))
-#  define SRPC_NOTREACHED(a)
-#else  /* __sgi || ghs || ..... */
-#  define SRPC_NOTREACHED(a) a
-#endif /* __sgi || ghs || ..... */
+#define SRPC_UNUSED_ARG     avoid_unused_warning
 
 #endif // SRPC_SRPC_MACROS_H
