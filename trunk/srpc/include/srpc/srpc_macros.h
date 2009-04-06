ef4c20032d5202fca7cfb43e3aa6507be07a7b85
kcando <kcando@6d7ccee0-1a3b-0410-bfa1-83648d9ec9a4>
2009-04-06 05:37:20 +0000

    ADD: add namespace

--- trunk/srpc/include/srpc/srpc_macros.h
+++ trunk/srpc/include/srpc/srpc_macros.h
@@ -5,10 +5,15 @@
 #pragma once
 #endif
 
+namespace srpc
+{
+
 // stolen from boost/statechart/detail/avoid_unused_warning.hpp
 template< typename T >
 inline void avoid_unused_warning(const T &) {}
 
-#define SRPC_UNUSED_ARG     avoid_unused_warning
+} // namespace srpc
+
+#define SRPC_UNUSED_ARG     srpc::avoid_unused_warning
 
 #endif // SRPC_SRPC_MACROS_H
